54dc010bdabf15508e666aaad6d16bb6b4e4ac8d
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2022-02-22 17:53:16 +0700

    Add calendar

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonChip, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const today = new Date();
+  const dateArr = [];
+
+  for (let i = -1; i <= 1; i++) {
+    const month =
+      today.getMonth() + 1 < 10
+        ? '0' + (today.getMonth() + 1)
+        : today.getMonth() + 1;
+    const fullDateString = `${today.getFullYear()}-${month}-${today.getDate() + i}`;
+    dateArr.push({
+      date: today.getDate() + i,
+      month: new Date(fullDateString).toLocaleString('en-US', {
+        month: 'short',
+      }),
+      fullDateString,
+    });
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Matches</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="calendar">
+          {
+            dateArr.map((date) => (
+              <IonButton color="primary" key={date.date}>{date.date} {date.month}</IonButton>
+            ))
+          }
+        </div>
+        <div className="leagues">
+          <IonChip color="primary">
+            <IonLabel>Secondary Label</IonLabel>
+          </IonChip>
+        </div>
       </IonContent>
     </IonPage>
   );
