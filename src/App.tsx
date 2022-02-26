fbebaf9a56290f61c1312d8d0431f2c06f960c52
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2022-02-26 20:41:34 +0700

    Fix unused var error

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import { IonReactRouter } from '@ionic/react-router';
 import { footballOutline, listOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
